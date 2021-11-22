4143f68fd5c3ae1a746b528def0ba1c5f13b9a28
Your Name <you@example.com>
2021-11-22 22:05:29 +0530

    funcational interface

---
+++ ConsumerExamples1/src/ConsumerExample.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample implements Consumer<String> {
+
+	public static void main(String[] args) {
+		ConsumerExample consumerExample = new ConsumerExample();
+		consumerExample.accept("Chandu");
+
+		Consumer<String> result = t -> System.out.println("hello: " + t);
+		result.accept("Pranith");
+
+		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6);
+		data.forEach(d -> System.out.println(d));
+	}
+
+	@Override
+	public void accept(String t) {
+		System.out.println("hello: " + t);
+	}
+
+}
---
+++ PredicateExample1/src/PredicateExample1.java
@@ -0,0 +1,23 @@
+import java.util.function.Predicate;
+
+public class PredicateExample1 implements Predicate<Integer> {
+
+	@Override
+	public boolean test(Integer t) {
+
+		if (t % 2 == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		Predicate<Integer> result = new PredicateExample1();
+		System.out.println(result.test(4));
+
+		Predicate<Integer> result1 = t -> (t % 2 == 0);
+		System.out.println(result1.test(6));
+	}
+}
